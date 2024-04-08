49d07bdca7de8cddf92f3fb0b63d0d1d1f89a43d
Stacy <salyiela@icloud.com>
2024-04-08 15:14:17 +0300

    Complete lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// function addingEventListener() {
+//     document.querySelector('#input').addEvenetListener('click', clickAlert)
+// }
+
+// const clickAlert = () => alert("I was clicked")
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
 }
+
+addingEventListener();
+// function clickAlert(){
+//     return alert('Hi I was clicked!')
+// };
